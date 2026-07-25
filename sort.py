33f742b985baf5fa6dcd58fa80d63eeb9fe6d411
Jeremy Gao <wensimehrp@gmail.com>
2026-07-25 05:30:42 -0700

    feat: add sort csv script

---
+++ sort.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env -S uv run
+with open("out.csv", "r", encoding="utf-8") as f:
+    data = f.readlines()[1:]
+data.sort()
+with open("out.sorted", "w", encoding="utf-8") as f:
+    _ = f.write("station_name,lon,lat,id\n")
+    f.writelines(data)
