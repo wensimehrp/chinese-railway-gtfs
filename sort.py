379d52332bcb552c9eebf329e227c2abe401e43b
Jeremy Gao <wensimehrp@gmail.com>
2026-07-25 22:21:21 -0700

    fix: don't add id header in sort

--- sort.py
+++ sort.py
@@ -3,5 +3,5 @@ with open("out.csv", "r", encoding="utf-8") as f:
     data = f.readlines()[1:]
 data.sort()
 with open("out.csv", "w", encoding="utf-8") as f:
-    _ = f.write("station_name,lon,lat,id\n")
+    _ = f.write("station_name,lon,lat\n")
     f.writelines(data)
